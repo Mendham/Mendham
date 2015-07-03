00efaf2cb93198736206d84e7267eb454d2c5314
cbeall <cbeall@users.noreply.github.com>
2015-07-03 17:27:30 -0400

    adding object extensions without tests to see how it works when loading new

---
+++ src/Mendham/ObjectExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CBeall.Mendham
+{
+	public static class ObjectExtensions
+	{
+		/// <summary>
+		/// Converts T into an IEnumerable&lt;T> that has the original object as its only item
+		/// </summary>
+		/// <typeparam name="T">Type of enumerable</typeparam>
+		/// <param name="obj">Object to add to collection</param>
+		/// <returns></returns>
+		public static IEnumerable<T> AsSingleItemEnumerable<T>(this T obj)
+		{
+			return new List<T> { obj };
+		}
+	}
+}
