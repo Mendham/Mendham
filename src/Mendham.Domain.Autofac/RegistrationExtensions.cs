9d3d40991551b508a144fbcbb419d1f2cda8ffe9
cbeall <cbeall@users.noreply.github.com>
2015-07-12 21:33:47 -0400

    started working on RegistrationExtensions... need to verify an dwrite tests... its still a WIP

---
+++ src/Mendham.Domain.Autofac/RegistrationExtensions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using Autofac;
+using Mendham.Domain.Events;
+
+namespace Mendham.Domain.Autofac
+{
+	public static class RegistrationExtensions
+	{
+		public static void RegisterDomainEventHandlers(this ContainerBuilder builder, Assembly assembly)
+		{
+			builder
+				.RegisterAssemblyTypes(assembly)
+				.As<IDomainEventHandler>();
+		}
+
+		public static void RegisterDomainFacades(this ContainerBuilder builder, Assembly assembly)
+		{
+			builder
+				.RegisterAssemblyTypes(assembly)
+				.Where(a => a
+					.GetTypeInfo()
+					.IsAssignableFrom(typeof(IDomainFacade).GetTypeInfo()))
+				.As(t => t.GetInterfaces()
+					.Where(a => a != typeof(IDomainFacade)));
+        }
+
+	}
+}
