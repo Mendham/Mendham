fd3148613da0b0a0c2d78ea3dcfa002f1835e3f6
cbeall <cbeall@users.noreply.github.com>
2015-07-14 07:40:02 -0400

    Got the first test working that verifies the domain event handlers are loading

---
+++ test/Mendham.Domain.Autofac.Test/Data/DomainEventHandlers.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Mendham.Domain.Events;
+
+namespace Mendham.Domain.Autofac.Test.Data
+{
+	public sealed class Test1DomainEventHandler : BaseDomainEventHandler<Test1DomainEvent>
+	{
+		public override Task HandleAsync(Test1DomainEvent domainEvent)
+		{
+			return Task.FromResult(0);
+		}
+	}
+
+	public sealed class Test2DomainEventHandler : BaseDomainEventHandler<Test2DomainEvent>
+	{
+		public override Task HandleAsync(Test2DomainEvent domainEvent)
+		{
+			return Task.FromResult(0);
+		}
+	}
+
+	public sealed class Test1DomainEvent : DomainEvent
+	{ }
+
+	public sealed class Test2DomainEvent : DomainEvent
+	{ }
+}
---
+++ test/Mendham.Domain.Autofac.Test/RegistrationExtensionsTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using Autofac;
+using FluentAssertions;
+using Mendham.Domain.Autofac.Test.Data;
+using Mendham.Domain.Events;
+using Xunit;
+
+namespace Mendham.Domain.Autofac.Test
+{
+	public class RegistrationExtensionsTest
+	{
+		[Fact]
+		public void RegisterDomainEventHandlers_HandlersInAssembly_ReturnsAll()
+		{
+			var assembly = typeof(RegistrationExtensionsTest).GetTypeInfo().Assembly;
+
+			var builder = new ContainerBuilder();
+			builder.RegisterDomainEventHandlers(assembly);
+
+			using (var sut = builder.Build().BeginLifetimeScope())
+			{
+				var result = sut.Resolve<IEnumerable<IDomainEventHandler>>();
+
+				result.Should()
+					.NotBeEmpty();
+				result.Should()
+					.ContainItemsAssignableTo<IDomainEventHandler>();
+				result.Should()
+					.Contain(a => a is Test1DomainEventHandler);
+				result.Should()
+					.Contain(a => a is Test2DomainEventHandler);
+			}
+		}
+    }
+}
