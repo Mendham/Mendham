917d087ae2a301972f2817b169614ca4b8041f36
cbeall <cbeall@users.noreply.github.com>
2015-07-27 07:41:33 -0400

    Completed the fixture for the database testing

---
+++ test/Mendham.Infrastructure.Dapper.Test/ConnectionWithSetTest.cs
@@ -0,0 +1,17 @@
+﻿using Mendham.Infrastructure.Dapper.Test.Fixtures;
+using Mendham.Testing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Mendham.Infrastructure.Dapper.Test
+{
+    public class ConnectionWithSetTest : BaseUnitTest<DatabaseFixture>
+    {
+        public ConnectionWithSetTest(DatabaseFixture fixture) : base(fixture)
+        {
+        }
+    }
+}
