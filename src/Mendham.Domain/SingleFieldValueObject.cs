beaad5bbf20fb43b78d2012eb5b42d4dcc2f27c1
cbeall <cbeall@users.noreply.github.com>
2015-11-03 07:42:26 -0500

    moved SingleFieldValueObject to its own file

---
+++ src/Mendham.Domain/SingleFieldValueObject.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mendham.Domain
+{
+    public abstract class SingleFieldValueObject<T> : ValueObject
+    {
+        public T Value { get; protected set; }
+
+        public SingleFieldValueObject(T value)
+        {
+            value.VerifyArgumentNotDefaultValue("Value is required");
+            this.Value = value;
+        }
+
+        public static implicit operator T(SingleFieldValueObject<T> singleFieldValueObject)
+        {
+            return singleFieldValueObject.Value;
+        }
+
+        protected override IEnumerable<object> EqualityComponents
+        {
+            get
+            {
+                yield return this.Value;
+            }
+        }
+
+        public override string ToString()
+        {
+            return Value.ToString();
+        }
+    }
+}
