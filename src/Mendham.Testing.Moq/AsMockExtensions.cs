46d8b82982304735d11c331d097202e7be629d31
cbeall <cbeall@users.noreply.github.com>
2016-01-30 19:37:40 -0500

    Renamed Moq Extension classes

---
+++ src/Mendham.Testing.Moq/AsMockExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Moq;
+using Moq.Language.Flow;
+
+namespace Mendham.Testing.Moq
+{
+	public static class AsMockExtensions
+	{
+        /// <summary>
+        /// Converts an object of Mock<T> to a Mock<T>"/>
+        /// </summary>
+        /// <typeparam name="T">Type of object</typeparam>
+        /// <param name="obj">Object of Mock<T></param>
+        /// <returns>Mock of the object</returns>
+		public static Mock<T> AsMock<T>(this T obj)
+			where T : class
+		{
+			return Mock.Get(obj);
+		}
+	}
+}
--- src/Mendham.Testing.Moq/ReturnExtensions.cs
+++ src/Mendham.Testing.Moq/ReturnExtensions.cs
@@ -1,25 +1,13 @@
-﻿using System.Collections.Generic;
+﻿using Moq.Language.Flow;
+using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Moq;
-using Moq.Language.Flow;
 
 namespace Mendham.Testing.Moq
 {
-	public static class MoqExtensions
-	{
-        /// <summary>
-        /// Converts an object of Mock<T> to a Mock<T>"/>
-        /// </summary>
-        /// <typeparam name="T">Type of object</typeparam>
-        /// <param name="obj">Object of Mock<T></param>
-        /// <returns>Mock of the object</returns>
-		public static Mock<T> AsMock<T>(this T obj)
-			where T : class
-		{
-			return Mock.Get(obj);
-		}
-
+    public static class ReturnExtensions
+    {
         /// <summary>
         /// For a method that returns a enumeration, returns items (passed as parameters) as a collection of those items
         /// </summary>
@@ -54,10 +42,10 @@ namespace Mendham.Testing.Moq
         /// <param name="setup">Setup of mock</param>
         /// <param name="results">Set of items to be returned on subsequent calls</param>
 		public static void ReturnsInOrder<T, TResult>(this ISetup<T, TResult> setup, params TResult[] results)
-			where T : class
-		{
-			setup.Returns(new Queue<TResult>(results).Dequeue);
-		}
+            where T : class
+        {
+            setup.Returns(new Queue<TResult>(results).Dequeue);
+        }
 
         /// <summary>
         /// Allows for a mock setup to return a different result on subsequent calls based as defined by the collection of results
@@ -67,17 +55,17 @@ namespace Mendham.Testing.Moq
         /// <param name="setup">Setup of mock</param>
         /// <param name="results">Set of items to be returned on subsequent calls</param>
 		public static void ReturnsInOrderAsync<TMock, TResult>(this ISetup<TMock, Task<TResult>> setup, params TResult[] results)
-			where TMock : class
-		{
-			var taskResults = results.Select(a =>
-			{
-				var tcs = new TaskCompletionSource<TResult>();
-				tcs.SetResult(a);
-				return tcs.Task;
-			});
+            where TMock : class
+        {
+            var taskResults = results.Select(a =>
+            {
+                var tcs = new TaskCompletionSource<TResult>();
+                tcs.SetResult(a);
+                return tcs.Task;
+            });
 
-			setup.Returns(new Queue<Task<TResult>>(taskResults).Dequeue);
-		}
+            setup.Returns(new Queue<Task<TResult>>(taskResults).Dequeue);
+        }
 
         /// <summary>
         /// Returns an empty task for a ISetup<TMock, Task>
@@ -85,9 +73,9 @@ namespace Mendham.Testing.Moq
         /// <typeparam name="TMock">Type of Mock</typeparam>
         /// <param name="setup">Setup of Mock that expects a return type of Task</param>
 		public static void ReturnsNoActionTask<TMock>(this ISetup<TMock, Task> setup)
-			where TMock : class
-		{
-			setup.Returns(Task.FromResult(0));
-		}
-	}
+            where TMock : class
+        {
+            setup.Returns(Task.FromResult(0));
+        }
+    }
 }
