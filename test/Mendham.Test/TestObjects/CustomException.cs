07b5ffb3f3a23ee740b530d544d7141ef4b45eb7
cbeall <cbeall@users.noreply.github.com>
2016-01-31 08:40:32 -0500

    Added all of the tests for the new verification stubs

---
+++ test/Mendham.Test/TestObjects/CustomException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mendham.Test.TestObjects
+{
+    public class CustomException : Exception
+    {
+        public CustomException(string message)
+            :base(message)
+        { }
+    }
+}
