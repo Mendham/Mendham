cd367055993d31ab76601d8fedccb7c9564d92e5
cbeall <cbeall@users.noreply.github.com>
2016-02-10 06:50:54 -0500

    Added comment on Int64ConcurrencyToken

--- src/Mendham.Concurrency/Int64ConcurrencyToken.cs
+++ src/Mendham.Concurrency/Int64ConcurrencyToken.cs
@@ -1,12 +1,9 @@
-﻿using Mendham;
-using System;
-using System.Collections.Generic;
+﻿using System;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace Mendham.Concurrency
 {
-	public class Int64ConcurrencyToken : IConcurrencyToken, IComparable
+    public class Int64ConcurrencyToken : IConcurrencyToken, IComparable
     {
         private readonly long tokenValue;
         private readonly byte[] byteValue;
@@ -31,11 +28,17 @@ namespace Mendham.Concurrency
             this.tokenValue = tokenValue;
         }
 
+        /// <summary>
+        /// Int64 value for concurrency token
+        /// </summary>
         public long Value
         {
             get { return tokenValue; }
         }
 
+        /// <summary>
+        /// Byte[] value of the concurrency token
+        /// </summary>
         public byte[] Bytes
         {
             get { return byteValue; }
@@ -104,6 +107,12 @@ namespace Mendham.Concurrency
             return Value.CompareTo(otherInt64ConcurrencyToken.Value);
         }
 
+        /// <summary>
+        /// Compares this Int64ConcurrencyToken with other to determine if it is at least as current (equal or
+        /// more recent) than the other Int64ConcurrencyToken
+        /// </summary>
+        /// <param name="other"></param>
+        /// <returns>True if the values are the same or if this concurrency token is newer.  False if older.</returns>
         public bool IsAtLeastAsCurrentAs(Int64ConcurrencyToken other)
         {
             return Value.CompareTo(other.Value) >= 0;
