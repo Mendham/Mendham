b446a54b920435c6e78904437d19eb1ba2cc50ac
cbeall <cbeall@users.noreply.github.com>
2016-02-09 21:16:56 -0500

    Moved location of ConcurrencyExtensionsTest

--- test/Mendham.Concurrency.Test/ConcurrencyExtensionsTest.cs
+++ test/Mendham.Concurrency.Test/ConcurrencyExtensionsTest.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Xunit;
 
-namespace Mendham.Concurrency.Test.TestObjects
+namespace Mendham.Concurrency.Test
 {
     public class ConcurrencyExtensionsTest
     {
