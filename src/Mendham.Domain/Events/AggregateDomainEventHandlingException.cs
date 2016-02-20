94d49c07290878bc1a27dbc6fed255f142e1536c
cbeall <cbeall@users.noreply.github.com>
2016-02-20 12:41:08 -0500

    MOved AggregateDomainEventHandlingException into its own file

---
+++ src/Mendham.Domain/Events/AggregateDomainEventHandlingException.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Linq;
+
+namespace Mendham.Domain.Events
+{
+    /// <summary>
+	/// An exception that contains details when multiple domain event handlers throw an excpetion
+	/// during the processing of a single domain event being raised.
+	/// </summary>
+	[DebuggerDisplay("Count = {Count}")]
+    public class AggregateDomainEventHandlingException : DomainEventHandlingException
+    {
+        private readonly IReadOnlyCollection<DomainEventHandlingException> domainEventHandlingExceptions;
+
+        internal AggregateDomainEventHandlingException(IEnumerable<DomainEventHandlingException> domainEventHandlingExceptions, DomainEventHandlingException firstException)
+            : base(firstException)
+        {
+            domainEventHandlingExceptions
+                .VerifyArgumentNotNullOrEmpty(nameof(domainEventHandlingExceptions), "The exceptions passed cannot be null or empty")
+                .VerifyArgumentMeetsCriteria(a => a.Count() > 1,
+                nameof(domainEventHandlingExceptions), "AggregateDomainEventHandlingException more than one exception")
+                .VerifyArgumentMeetsCriteria(a => a
+                    .Select(b => b.DomainEvent)
+                    .Distinct()
+                    .Count() == 1, nameof(domainEventHandlingExceptions), "The exceptions passed do not all have a matching domain event");
+
+            this.DomainEvent = firstException.DomainEvent;
+            this.DomainEventHandlerType = firstException.DomainEventHandlerType;
+
+            this.domainEventHandlingExceptions = new ReadOnlyCollection<DomainEventHandlingException>(domainEventHandlingExceptions.ToList());
+        }
+
+        /// <summary>
+        /// Types of the domain event handlers that threw an exception
+        /// </summary>
+        public IEnumerable<Type> DomainEventHandlerTypes
+        {
+            get
+            {
+                return domainEventHandlingExceptions
+                    .Select(a => a.DomainEventHandlerType);
+            }
+        }
+
+        /// <summary>
+        /// Exceptions thrown by domain event handlers
+        /// </summary>
+        public IEnumerable<DomainEventHandlingException> InnerExceptions
+        {
+            get
+            {
+                return domainEventHandlingExceptions;
+            }
+        }
+
+        public override string Message
+        {
+            get
+            {
+                return string.Format("Multiple exceptions occured when handling the domain event. Exception count {0}. See See INNER EXCEPTIONS for details.",
+                    this.domainEventHandlingExceptions.Count());
+            }
+        }
+    }
+}
--- src/Mendham.Domain/Events/DomainEventHandlingException.cs
+++ src/Mendham.Domain/Events/DomainEventHandlingException.cs
@@ -1,17 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
-using Mendham;
 
 namespace Mendham.Domain.Events
 {
-	/// <summary>
-	/// Exception that occurs when an event handler throws an exception when an event is rasied.
-	/// The inner exception contains the details of the exception.
-	/// </summary>
+    /// <summary>
+    /// Exception that occurs when an event handler throws an exception when an event is rasied.
+    /// The inner exception contains the details of the exception.
+    /// </summary>
     public class DomainEventHandlingException : Exception
     {
 		/// <summary>
@@ -46,66 +40,6 @@ namespace Mendham.Domain.Events
 				return string.Format("{0} {1}. See INNER EXCEPTION for details",
 					DEFAULT_MESSAGE,
 					DomainEventHandlerType.FullName);
-			}
-		}
-	}
-
-	/// <summary>
-	/// An exception that contains details when multiple domain event handlers throw an excpetion
-	/// during the processing of a single domain event being raised.
-	/// </summary>
-	[DebuggerDisplay("Count = {Count}")]
-	public class AggregateDomainEventHandlingException : DomainEventHandlingException
-	{
-		private readonly IReadOnlyCollection<DomainEventHandlingException> domainEventHandlingExceptions;
-
-		internal AggregateDomainEventHandlingException(IEnumerable<DomainEventHandlingException> domainEventHandlingExceptions, DomainEventHandlingException firstException)
-			:base(firstException)
-		{
-			domainEventHandlingExceptions
-				.VerifyArgumentNotNullOrEmpty(nameof(domainEventHandlingExceptions), "The exceptions passed cannot be null or empty")
-				.VerifyArgumentMeetsCriteria(a => a.Count() > 1,
-                nameof(domainEventHandlingExceptions), "AggregateDomainEventHandlingException more than one exception")
-                .VerifyArgumentMeetsCriteria(a => a
-                    .Select(b => b.DomainEvent)
-                    .Distinct()
-                    .Count() == 1, nameof(domainEventHandlingExceptions), "The exceptions passed do not all have a matching domain event");
-
-			this.DomainEvent = firstException.DomainEvent;
-			this.DomainEventHandlerType = firstException.DomainEventHandlerType;
-
-            this.domainEventHandlingExceptions = new ReadOnlyCollection<DomainEventHandlingException>(domainEventHandlingExceptions.ToList());
-        }
-
-		/// <summary>
-		/// Types of the domain event handlers that threw an exception
-		/// </summary>
-		public IEnumerable<Type> DomainEventHandlerTypes
-		{
-			get
-			{
-				return domainEventHandlingExceptions
-					.Select(a => a.DomainEventHandlerType);
-			}
-		}
-		
-		/// <summary>
-		/// Exceptions thrown by domain event handlers
-		/// </summary>
-		public IEnumerable<DomainEventHandlingException> InnerExceptions
-		{
-			get
-			{
-				return domainEventHandlingExceptions;
-			}
-		}
-
-		public override string Message
-		{
-			get
-			{
-				return string.Format("Multiple exceptions occured when handling the domain event. Exception count {0}. See See INNER EXCEPTIONS for details.",
-					this.domainEventHandlingExceptions.Count());
 			}
 		}
 	}
