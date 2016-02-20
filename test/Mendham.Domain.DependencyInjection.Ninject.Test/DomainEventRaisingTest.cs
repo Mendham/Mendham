3d9bdadf9be045266c5cf848cefff7d9f97458d4
cbeall <cbeall@users.noreply.github.com>
2016-02-20 12:35:38 -0500

    Added additinoal domain event tests for ninject

--- test/Mendham.Domain.DependencyInjection.Ninject.Test/DomainEventRaisingTest.cs
+++ test/Mendham.Domain.DependencyInjection.Ninject.Test/DomainEventRaisingTest.cs
@@ -13,6 +13,49 @@ namespace Mendham.Domain.DependencyInjection.Ninject.Test
     public class DomainEventRaisingTest
     {
         [Fact]
+        public async Task Raise_SingleEvent_IsLogged()
+        {
+            using (var kernel = new StandardKernel(new DomainEventHandlingModule()))
+            {
+                kernel.RegisterDomainEventHandlers(GetType().Assembly);
+                kernel.Bind<IDomainEventLogger>().To<TestDomainEventLogger>().InSingletonScope();
+
+                var publisher = kernel.Get<IDomainEventPublisher>();
+                var logger = kernel.Get<IDomainEventLogger>() as TestDomainEventLogger;
+
+                var domainEvent = new Test1DomainEvent();
+
+                await publisher.RaiseAsync(domainEvent);
+
+                logger.LoggedEvents.Should()
+                    .HaveCount(1)
+                    .And.Contain(domainEvent);
+            }
+        }
+
+        [Fact]
+        public async Task Raise_SingleHandler_IsRaised()
+        {
+            using (var kernel = new StandardKernel(new DomainEventHandlingModule()))
+            {
+                kernel.RegisterDomainEventHandlers(GetType().Assembly);
+
+                var publisher = kernel.Get<IDomainEventPublisher>();
+                var handler = kernel.GetAll<IDomainEventHandler>()
+                    .OfType<WasCalledVerifiableHandler>()
+                    .Single();
+
+                var domainEvent = new WasCalledVerifiableEvent();
+
+                await publisher.RaiseAsync(domainEvent);
+
+                var result = handler.WasEverCalled;
+
+                result.Should().BeTrue();
+            }
+        }
+
+        [Fact]
         public async Task Raise_HandlerLogsSecondEvent_BothLogged()
         {
             using (var kernel = new StandardKernel(new DomainEventHandlingModule()))
---
+++ test/Mendham.Domain.DependencyInjection.Ninject.Test/TestObjects/WasCalledVerifiableHandler.cs
@@ -0,0 +1,27 @@
+﻿using Mendham.Domain.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mendham.Domain.DependencyInjection.Ninject.Test.TestObjects
+{
+    public sealed class WasCalledVerifiableHandler : DomainEventHandler<WasCalledVerifiableEvent>
+    {
+        public WasCalledVerifiableHandler()
+        {
+            this.WasEverCalled = false;
+        }
+
+        public bool WasEverCalled { get; private set; }
+
+        public override Task HandleAsync(WasCalledVerifiableEvent domainEvent)
+        {
+            WasEverCalled = true;
+            return Task.FromResult(0);
+        }
+    }
+
+    public sealed class WasCalledVerifiableEvent : DomainEvent
+    { }
+}
