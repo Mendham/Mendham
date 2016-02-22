c0cdae651bd1555dcebc1c529131bc2c2faa3fa5
cbeall <cbeall@users.noreply.github.com>
2016-02-21 23:18:50 -0500

    Added tests for DomainEventLoggerProcessor

---
+++ test/Mendham.Domain.Test/DomainEventLoggerProcessorTest.cs
@@ -0,0 +1,38 @@
+﻿using Mendham.Domain.Test.Fixtures;
+using Mendham.Testing;
+using Mendham.Testing.Moq;
+using Moq;
+using Xunit;
+
+namespace Mendham.Domain.Test
+{
+    public class DomainEventLoggerProcessorTest : UnitTest<DomainEventLoggerProcessorFixture>
+    {
+        public DomainEventLoggerProcessorTest(DomainEventLoggerProcessorFixture fixture) : base(fixture)
+        { }
+
+        [Fact]
+        public void LogDomainEventRaised_MultipleLoggers_AllLogged()
+        {
+            Fixture.AddLogger(Fixture.DomainEventLogger1);
+            Fixture.AddLogger(Fixture.DomainEventLogger2);
+
+            var sut = Fixture.CreateSut();
+
+            sut.LogDomainEvent(Fixture.DomainEvent);
+
+            Fixture.DomainEventLogger1.AsMock()
+                .Verify(a => a.LogDomainEventRaised(Fixture.DomainEvent), Times.Once());
+            Fixture.DomainEventLogger2.AsMock()
+                .Verify(a => a.LogDomainEventRaised(Fixture.DomainEvent), Times.Once());
+        }
+
+        [Fact]
+        public void LogDomainEventRaised_NoLoggers_Completes()
+        {
+            var sut = Fixture.CreateSut();
+
+            sut.LogDomainEvent(Fixture.DomainEvent);
+        }
+    }
+}
---
+++ test/Mendham.Domain.Test/Fixtures/DomainEventLoggerProcessorFixture.cs
@@ -0,0 +1,46 @@
+﻿using Mendham.Domain.Events;
+using Mendham.Domain.Events.Components;
+using Mendham.Domain.Test.TestObjects.Events;
+using Mendham.Testing;
+using Moq;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Mendham.Domain.Test.Fixtures
+{
+    public class DomainEventLoggerProcessorFixture : Fixture<DomainEventLoggerProcessor>
+    {
+        public IDomainEventLogger DomainEventLogger1 { get; set; }
+        public IDomainEventLogger DomainEventLogger2 { get; set; }
+
+        private List<IDomainEventLogger> loggers;
+
+        public DomainEventLoggerProcessorFixture()
+        {
+            loggers = new List<IDomainEventLogger>();
+        }
+
+        public TestDomainEvent DomainEvent { get; set; }
+
+        public override DomainEventLoggerProcessor CreateSut()
+        {
+            var loggersAtCreation = loggers.ToList();
+            return new DomainEventLoggerProcessor(() => loggersAtCreation);
+        }
+
+        public void AddLogger(IDomainEventLogger logger)
+        {
+            loggers.Add(logger);
+        }
+
+        public override void ResetFixture()
+        {
+            base.ResetFixture();
+
+            loggers.Clear();
+
+            DomainEventLogger1 = Mock.Of<IDomainEventLogger>();
+            DomainEventLogger2 = Mock.Of<IDomainEventLogger>();
+        }
+    }
+}
