dd4be67480a88ed613ac11f6adc2d53e59c682b4
cbeall <cbeall@users.noreply.github.com>
2016-08-21 16:41:36 -0400

    Crated empty Mendham.Testing.Events and renamed Mendham.Testing.Domain.Test to Mendham.Testing.Events.Test.

--- src/Mendham.Testing.Events/Properties/AssemblyInfo.cs
+++ src/Mendham.Testing.Events/Properties/AssemblyInfo.cs
@@ -5,14 +5,10 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Mendham.Testing.Domain.Test")]
-[assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Mendham.Testing.Domain.Test")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyProduct("Mendham.Testing.Events")]
 [assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
@@ -20,4 +16,4 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("3a07ba56-9fba-4d95-bab0-7bc38d7f3327")]
+[assembly: Guid("96a07ae4-c95c-47bb-9155-d8a835950743")]
