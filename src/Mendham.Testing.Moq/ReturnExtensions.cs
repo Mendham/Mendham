c49b8e2c0c8f5ab42b22cf7389b3ddec622f9667
cbeall <cbeall@users.noreply.github.com>
2016-08-27 11:39:10 -0400

    Modified the return type of the return extensions

--- src/Mendham.Testing.Moq/ReturnExtensions.cs
+++ src/Mendham.Testing.Moq/ReturnExtensions.cs
@@ -15,10 +15,10 @@ namespace Mendham.Testing.Moq
         /// <typeparam name="TResult">Type of results returned by setup</typeparam>
         /// <param name="setup">Setup of mock</param>
         /// <param name="results">Additional items to be returned</param>
-		public static void ReturnsItems<T, TResult>(this ISetup<T, IEnumerable<TResult>> setup, params TResult[] results)
+		public static IReturnsResult<T> ReturnsItems<T, TResult>(this ISetup<T, IEnumerable<TResult>> setup, params TResult[] results)
             where T : class
         {
-            setup.Returns(results);
+            return setup.Returns(results);
         }
 
         /// <summary>
@@ -28,10 +28,10 @@ namespace Mendham.Testing.Moq
         /// <typeparam name="TResult">Type of results returned by setup</typeparam>
         /// <param name="setup">Setup of mock</param>
         /// <param name="results">Additional items to be returned</param>
-		public static void ReturnsItemsAsync<T, TResult>(this ISetup<T, Task<IEnumerable<TResult>>> setup, params TResult[] results)
+		public static IReturnsResult<T> ReturnsItemsAsync<T, TResult>(this ISetup<T, Task<IEnumerable<TResult>>> setup, params TResult[] results)
             where T : class
         {
-            setup.Returns(Task.FromResult<IEnumerable<TResult>>(results));
+            return setup.Returns(Task.FromResult<IEnumerable<TResult>>(results));
         }
 
         /// <summary>
@@ -41,10 +41,10 @@ namespace Mendham.Testing.Moq
         /// <typeparam name="TResult">Type of results returned by setup</typeparam>
         /// <param name="setup">Setup of mock</param>
         /// <param name="results">Set of items to be returned on subsequent calls</param>
-		public static void ReturnsInOrder<T, TResult>(this ISetup<T, TResult> setup, params TResult[] results)
+		public static IReturnsResult<T> ReturnsInOrder<T, TResult>(this ISetup<T, TResult> setup, params TResult[] results)
             where T : class
         {
-            setup.Returns(new Queue<TResult>(results).Dequeue);
+            return setup.Returns(new Queue<TResult>(results).Dequeue);
         }
 
         /// <summary>
@@ -54,7 +54,7 @@ namespace Mendham.Testing.Moq
         /// <typeparam name="TResult">Type of results returned by setup</typeparam>
         /// <param name="setup">Setup of mock</param>
         /// <param name="results">Set of items to be returned on subsequent calls</param>
-		public static void ReturnsInOrderAsync<TMock, TResult>(this ISetup<TMock, Task<TResult>> setup, params TResult[] results)
+		public static IReturnsResult<TMock> ReturnsInOrderAsync<TMock, TResult>(this ISetup<TMock, Task<TResult>> setup, params TResult[] results)
             where TMock : class
         {
             var taskResults = results.Select(a =>
@@ -64,18 +64,18 @@ namespace Mendham.Testing.Moq
                 return tcs.Task;
             });
 
-            setup.Returns(new Queue<Task<TResult>>(taskResults).Dequeue);
+            return setup.Returns(new Queue<Task<TResult>>(taskResults).Dequeue);
         }
 
         /// <summary>
         /// Returns an empty task for a ISetup<TMock, Task>
         /// </summary>
-        /// <typeparam name="TMock">Type of Mock</typeparam>
+        /// <typeparam name="T">Type of Mock</typeparam>
         /// <param name="setup">Setup of Mock that expects a return type of Task</param>
-		public static void ReturnsNoActionTask<TMock>(this ISetup<TMock, Task> setup)
-            where TMock : class
+		public static IReturnsResult<T> ReturnsNoActionTask<T>(this ISetup<T, Task> setup)
+            where T : class
         {
-            setup.Returns(Task.FromResult(0));
+            return setup.Returns(Task.FromResult(0));
         }
     }
 }
