56be7b00f058d018bb98e765d48a8038683f600c
cbeall <cbeall@users.noreply.github.com>
2016-08-28 09:52:01 -0400

    Added event publisher components for AspNetCore support

---
+++ src/Mendham.Events.DependencyInjection.AspNetCore/RegistrationExtensions.cs
@@ -0,0 +1,33 @@
+﻿using Mendham.Events;
+using Mendham.Events.Components;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using System;
+using System.Collections.Generic;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class RegistrationExtensions
+    {
+        public static void AddEventHandling(this IServiceCollection services)
+        {
+            services.AddTransient<IEventHandlerContainer, DefaultEventHandlerContainer>();
+            services.AddTransient<Func<IEnumerable<IEventHandler>>>(serviceProvider =>
+                () => serviceProvider.GetServices<IEventHandler>());
+
+            services.AddTransient<IEventLoggerProcessor, EventLoggerProcessor>();
+            services.AddTransient<Func<IEnumerable<IEventLogger>>>(serviceProvider =>
+                () => serviceProvider.GetServices<IEventLogger>());
+
+            services.AddTransient<IEventHandlerProcessor, EventHandlerProcessor>();
+
+            services.AddTransient<IEventPublisherComponents, EventPublisherComponents>();
+
+            services.AddTransient<IEventLoggerProcessor>(serviceProvider =>
+                new EventLoggerProcessor(() => serviceProvider.GetServices<IEventLogger>()));
+
+            services.AddTransient<IEventHandlerProcessor, EventHandlerProcessor>();
+
+            services.TryAddTransient<IEventPublisher, EventPublisher>();
+        }
+    }
+}
---
+++ test/Mendham.Events.DependencyInjection.AspNetCore.Test/RegistrationExtensionsTest.cs
@@ -0,0 +1,109 @@
+﻿using FluentAssertions;
+using Mendham.Events.Components;
+using Mendham.Events.DependencyInjection.TestObjects;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Mendham.Events.DependencyInjection.AspNetCore.Test
+{
+    public class RegistrationExtensionsTest
+    {
+        [Fact]
+        public void AddEventHandling_GetEventPublisher_Resolves()
+        {
+            var builder = new WebHostBuilder()
+                .ConfigureServices(sc => sc.AddEventHandling())
+                .Configure(app => { });
+
+            using (var server = new TestServer(builder))
+            {
+                var result = server.Host.Services.GetService<IEventPublisher>();
+
+                result.Should()
+                    .NotBeNull()
+                    .And.BeOfType<EventPublisher>();
+            }
+        }
+
+        [Fact]
+        public void AddEventHandling_GetEventHandlingContainer_Resolves()
+        {
+            var builder = new WebHostBuilder()
+                .ConfigureServices(sc => sc.AddEventHandling())
+                .Configure(app => { });
+
+            using (var server = new TestServer(builder))
+            {
+                var result = server.Host.Services.GetService<IEventHandlerContainer>();
+
+                result.Should()
+                    .NotBeNull()
+                    .And.BeOfType<DefaultEventHandlerContainer>();
+            }
+        }
+
+        [Fact]
+        public void AddEventHandling_GetEventHandlingProcessor_Resolves()
+        {
+            var builder = new WebHostBuilder()
+                .ConfigureServices(sc => sc.AddEventHandling())
+                .Configure(app => { });
+
+            using (var server = new TestServer(builder))
+            {
+                var result = server.Host.Services.GetService<IEventHandlerProcessor>();
+
+                result.Should()
+                    .NotBeNull()
+                    .And.BeOfType<EventHandlerProcessor>();
+            }
+        }
+
+        [Fact]
+        public void AddEventHandling_GetEventLoggerProcessor_Resolves()
+        {
+            var builder = new WebHostBuilder()
+                .ConfigureServices(sc => sc.AddEventHandling())
+                .Configure(app => { });
+
+            using (var server = new TestServer(builder))
+            {
+                var result = server.Host.Services.GetService<IEventLoggerProcessor>();
+
+                result.Should()
+                    .NotBeNull()
+                    .And.BeOfType<EventLoggerProcessor>();
+            }
+        }
+
+        [Fact]
+        public void AddEventHandling_RegisterEventPublisherAfterOther_UsesOtherEventPublisher()
+        {
+            var builder = new WebHostBuilder()
+                .ConfigureServices(sc => sc.AddTransient<IEventPublisher, AltEventPublisher>())
+                .ConfigureServices(sc => sc.AddEventHandling())
+                .Configure(app => { });
+
+            using (var server = new TestServer(builder))
+            {
+                var result = server.Host.Services.GetService<IEventPublisher>();
+
+                result.Should()
+                    .NotBeNull()
+                    .And.BeOfType<AltEventPublisher>();
+
+                // verify the other services load as expected (ConfigureServices runs twice)
+                var defaultEventProcessor = server.Host.Services.GetService<IEventLoggerProcessor>();
+                defaultEventProcessor.Should()
+                    .NotBeNull()
+                    .And.BeOfType<EventLoggerProcessor>();
+            }
+        }
+    }
+}
